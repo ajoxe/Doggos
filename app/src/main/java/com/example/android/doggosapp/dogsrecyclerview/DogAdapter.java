d0a72ae6465048556b4af57aad0ecd12af92af5b
Amirah Oxendine <amirahoxendine@ac.c4q.nyc>
2018-02-25 12:42:12 -0500

    app structure

---
+++ app/src/main/java/com/example/android/doggosapp/dogsrecyclerview/DogAdapter.java
@@ -0,0 +1,8 @@
+package com.example.android.doggosapp.dogsrecyclerview;
+
+/**
+ * Created by amirahoxendine on 2/25/18.
+ */
+
+public class DogAdapter {
+}
---
+++ app/src/main/java/com/example/android/doggosapp/model/Dog.java
@@ -0,0 +1,8 @@
+package com.example.android.doggosapp.model;
+
+/**
+ * Created by amirahoxendine on 2/25/18.
+ */
+
+public class Dog {
+}
