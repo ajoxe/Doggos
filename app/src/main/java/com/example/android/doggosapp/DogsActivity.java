5a95f7d8b3f2749e16502bad7bf40fb219c4d45d
Amirah Oxendine <amirahoxendine@ac.c4q.nyc>
2018-02-25 14:34:20 -0500

    menu struct

--- app/src/main/java/com/example/android/doggosapp/DogsActivity.java
+++ app/src/main/java/com/example/android/doggosapp/DogsActivity.java
@@ -2,6 +2,9 @@ package com.example.android.doggosapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 
 public class DogsActivity extends AppCompatActivity {
@@ -10,6 +13,19 @@ public class DogsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dogs);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.logout_options_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        //TODO shared prefs - delete username
+        return true;
     }
 
 }
